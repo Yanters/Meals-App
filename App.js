b6b8a68dea64dfe84db93e7d0db46b4910242360
Yanters <yanter@wp.pl>
2022-12-26 17:49:20 +0100

    nested navigation

--- App.js
+++ App.js
@@ -3,10 +3,35 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#541400',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        sceneContainerStyle: {
+          backgroundColor: '#572719',
+        },
+      }}
+    >
+      <Drawer.Screen name='Categories' component={CategoriesScreen} />
+      <Drawer.Screen name='Favorites' component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -28,10 +53,10 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name='Categories'
-            component={CategoriesScreen}
+            name='Drawer'
+            component={DrawerNavigator}
             options={{
-              title: 'Categories',
+              headerShown: false,
             }}
           />
           <Stack.Screen
