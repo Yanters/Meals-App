b1912a5d4e7b4852c98f598af829654b9c44ae66
Yanters <yanter@wp.pl>
2022-12-26 14:49:45 +0100

    main screen display

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,59 @@
+import { Pressable, Text, View, StyleSheet, Platform } from 'react-native';
+
+const CategoryGridTile = ({ title, color }) => {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: '#ccc' }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed && styles.buttonPressed,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    overflow: Platform.select({
+      ios: 'visible',
+      android: 'hidden',
+    }),
+    // Android shadow
+    elevation: 4,
+    // iOS shadow
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+};
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderCategoryItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoriesScreen;
+
